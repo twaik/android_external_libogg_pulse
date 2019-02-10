016641289b61c18b744a2f306a0517702f26038b
Twaik Yont <twaikyont@gmail.com>
2019-02-10 16:39:05 +0200

    Init commit

---
+++ Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include $(LOCAL_PATH)/android
+LOCAL_SRC_FILES := src/bitwise.c \
+                   src/framing.c
+LOCAL_MODULE    := libogg_pulse
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SDK_VERSION := 14
+include $(BUILD_STATIC_LIBRARY)
